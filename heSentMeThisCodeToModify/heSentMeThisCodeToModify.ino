b620028b2f8938942288bcf99c711ed5cd32a500
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2022-09-14 19:33:40 +0100

    fixed

--- heSentMeThisCodeToModify/heSentMeThisCodeToModify.ino
+++ heSentMeThisCodeToModify/heSentMeThisCodeToModify.ino
@@ -1,25 +1,52 @@
+/*<br>Sketch by SimonRob 
+3D printed equatorial mount for DSLR
+Instructables
+*/
+
+int buttonA4 = 0;
+int buttonA5 = 0;
+int ledOrange = 4;
+int ledRed = 3;
+
 #include <CheapStepper.h>
+
 CheapStepper stepper;
 
-#define ledOrange 4
-#define ledRed 3
-
 void setup() {
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
   pinMode(ledOrange, OUTPUT);
   pinMode(ledRed, OUTPUT);
 }
 
-boolean motor_turning = false;
 void loop() {
-  motor_turning = !digitalRead(A4) == HIGH && (digitalRead(A5) == HIGH || motor_turning);
-  
-  if(motor_turning){
-    stepper.step(false);
-    delay(3523);
-    digitalWrite (ledOrange, HIGH);
-    digitalWrite (ledRed, LOW);
-  } else {
-    digitalWrite (ledOrange, HIGH);
-    digitalWrite (ledRed, LOW);
-  }
+
+buttonA4 = digitalRead(A4);
+buttonA5 = digitalRead(A5);
+
+if (buttonA4 == HIGH){
+  buttonA4 = digitalRead(A4);
+  buttonA5 = digitalRead(A5);
+  digitalWrite (ledOrange, HIGH);
+  digitalWrite (ledRed, LOW);
+  stepper.step(false);
+  delay(6);
 }
+if (buttonA5 == HIGH){
+  buttonA4 = digitalRead(A4);
+  buttonA5 = digitalRead(A5);
+  digitalWrite (ledOrange, HIGH);
+  digitalWrite (ledRed, LOW);
+  stepper.step(true);
+  delay(6);
+}
+while((buttonA4 == LOW)&&(buttonA5 == LOW)){
+  buttonA4 = digitalRead(A4);
+  buttonA5 = digitalRead(A5);
+  digitalWrite (ledOrange, LOW);
+  digitalWrite (ledRed, HIGH);
+  stepper.step(false);
+  delay(3523);
+    
+}
+} 
