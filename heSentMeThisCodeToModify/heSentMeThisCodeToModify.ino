7476374a73dae18dfcd4cdedff04db60f5a76c44
K R E A M <45693917+iseegr8tfuldeadppl@users.noreply.github.com>
2022-09-14 18:55:20 +0100

    debugging

--- heSentMeThisCodeToModify/heSentMeThisCodeToModify.ino
+++ heSentMeThisCodeToModify/heSentMeThisCodeToModify.ino
@@ -5,11 +5,13 @@ CheapStepper stepper;
 #define ledRed 3
 
 void setup() {
+  Serial.begin(9600);
   pinMode(ledOrange, OUTPUT);
   pinMode(ledRed, OUTPUT);
 }
 
 void loop() {
+  Serial.println("A4: " + String(digitalRead(A4)) + " A5: " + String(digitalRead(A5)));
   if (digitalRead(A4) == HIGH){
     digitalWrite (ledOrange, HIGH);
     digitalWrite (ledRed, LOW);
